1c98d435c66c274dfaed08c4e5845131adbd6897
Caris Williams <cwill1013@gmail.com>
2024-02-01 16:55:01 -0500

    Initial Commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Caris";
+            int age = 42;
+            char middleInitial = 'R';
+            bool isOver18 = true;
+            double currentTemp = 58;
+            decimal currentPrice = 420.79m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+
         }
     }
 }
